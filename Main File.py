e45f70a8db3370e518af58224ad656974c5ffb18
Joshua Hutchins <ninjosh97@gmail.com>
2015-11-05 00:34:46 -0500

    Finess
    Cleaned up some more.
    Made the prints more friendly.
    Added comments.
    Added 1 to the naming order across the board so that the count would
    start at 1 not 0

--- Main File.py
+++ Main File.py
@@ -1,27 +1,32 @@
 __author__ = 'Josh'
 import os
-import time
-from os import path
 
+
+# Currently, the folder is determined programmatically. This method puts slashes between the terms.
 folder = os.path.join('C:\\', 'Users', 'Josh', 'Desktop', 'Images')
 
 
 def main():
-    newName = input("Please input a name for your photos")
-    print(folder)
+    # Collect a name from the user to name the files with
+    newName = input("Please input a name for your photos: ")
+    # Get the list of reordered files
     listOfFiles = getListOfFiles(folder)
-    print(listOfFiles)
+    # Rename the list of files with the new name
     renameFiles(listOfFiles, newName)
+    # Print a confirmation message!
+    print("The images have been renamed!")
 
-
+# This function returns all the listings in the directory(folderPath) and sorts them by the creation time using a dynamic(lambda) function.
 def getListOfFiles(folderPath):
     files = sorted(os.listdir(folderPath), key=lambda x: os.path.getctime(os.path.join(folderPath, x)))
     return files
 
-
+# This function renames all the files in the input list with the new name.
 def renameFiles(list, newName):
     for file in list:
+        # Seperate the filename from the extension. We need the extension in the next line.
         root, extension = os.path.splitext(file)
-        os.rename(os.path.join(folder, file), os.path.join(folder, newName + ' #' + str(list.index(file)) + extension))
+        # Rename the source(1st argument) path/file with the value of the second argument. The second argument adds the path to the new name, the order number, and the collected extenstion.
+        os.rename(os.path.join(folder, file), os.path.join(folder, newName + ' #' + str(list.index(file) + 1) + extension))
 
 main()
