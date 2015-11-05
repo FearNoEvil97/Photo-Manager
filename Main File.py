fa483a1262632b3ff1d0ec1f3fe52c3e2ce32065
Joshua Hutchins <ninjosh97@gmail.com>
2015-11-05 00:17:41 -0500

    IT WORKS!
    Added in the main logic to make it work, and it does. It renames all the
    images in a folder with a specified string and the number (order in
    which photos were taken).

--- Main File.py
+++ Main File.py
@@ -9,12 +9,19 @@ folder = os.path.join('C:\\', 'Users', 'Josh', 'Desktop', 'Images')
 def main():
     newName = input("Please input a name for your photos")
     print(folder)
-    print(getListOfFiles(folder))
+    listOfFiles = getListOfFiles(folder)
+    print(listOfFiles)
+    renameFiles(listOfFiles, newName)
 
 
 def getListOfFiles(folderPath):
     files = sorted(os.listdir(folderPath), key=lambda x: os.path.getctime(os.path.join(folderPath, x)))
     return files
 
+
+def renameFiles(list, newName):
+    for file in list:
+        root, extension = os.path.splitext(file)
+        os.rename(os.path.join(folder, file), os.path.join(folder, newName + ' #' + str(list.index(file)) + extension))
 
 main()
